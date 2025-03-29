5cbd3f08c767e4469d0f4c95dff579312398f4f3
Jeans <sistemadesignstyle@gmail.com>
2025-03-29 18:50:11 -0500

    configurando cors

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,16 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://tiendamaquimotora.enfocussoluciones.pe',
+        'https://admintiendamaquimotora.enfocussoluciones.pe',
+        'http://localhost:5000',
+        'http://localhost:4200'
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +34,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
